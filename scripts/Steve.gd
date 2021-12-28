0377c1d399053e0055a446ba93a34b079f0c044c
hiphurraa <61519166+hiphurraa@users.noreply.github.com>
2021-12-28 11:00:12 +0200

    moving in relation to camera angle

--- scripts/Steve.gd
+++ scripts/Steve.gd
@@ -2,9 +2,9 @@ extends KinematicBody
 
 var velocity = Vector3(0, 0, 0)
 const NORMAL_SPEED = 20
-const SPRINT_SPEED = 20
+const SPRINT_SPEED = 40
 const DIAGONAL_SCALE = 1.414
-const ACCELERATION = 0.05
+const ACCELERATION = 5
 
 var cameraTargetAngle = 0.0
 
@@ -13,158 +13,116 @@ func _ready():
 	pass
 	
 func _physics_process(delta):
+	print(delta)
 
 	# CAMERA
 	# ROTATE CAMERA LEFT
 	if Input.is_action_just_pressed("rotate_camera_left"):
-		if (cameraTargetAngle == 0):
-			$Camera_base.rotation_degrees.y = 360
-			cameraTargetAngle = 315
-		else:
-			cameraTargetAngle -= 45
-		print(cameraTargetAngle)
-	
-	# ROTATE CAMERA RIGHT		
-	if Input.is_action_just_pressed("rotate_camera_right"):
 		if (cameraTargetAngle == 360):
 			$Camera_base.rotation_degrees.y = 0
 			cameraTargetAngle = 45
 		else:
 			cameraTargetAngle += 45
 		print(cameraTargetAngle)
+	
+	# ROTATE CAMERA RIGHT		
+	if Input.is_action_just_pressed("rotate_camera_right"):
+		if (cameraTargetAngle == 0):
+			$Camera_base.rotation_degrees.y = 360
+			cameraTargetAngle = 315
+		else:
+			cameraTargetAngle -= 45
+		print(cameraTargetAngle)
+		
 		
 	$Camera_base.rotation_degrees.y = lerp($Camera_base.rotation_degrees.y, cameraTargetAngle, 0.1)
 	
-	
+	var dir = Vector3()
+	var inputMoveVector = Vector2()
+	var camera_dir = $Camera_base.get_global_transform().basis
 	
 	# PLAYER MOVEMENT
 	# SPEED
 	var speed = NORMAL_SPEED
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT_SPEED
-	
-	# X AXIS
-	if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_right"):
-		decelerate_x()
-	elif Input.is_action_pressed("move_right"):
-		if(cameraTargetAngle == 45):
-			pass
-		move_right(speed)
-	elif Input.is_action_pressed("move_left"):
-		move_left(speed)
-	else:
-		decelerate_x()
 		
-	# Z AXIS
+	# UP AND DOWN MOVEMENT
 	if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_down"):
-		decelerate_z()
+		pass
 	elif Input.is_action_pressed("move_up"):
-		move_up(speed)
+		inputMoveVector.y += 1
 	elif Input.is_action_pressed("move_down"):
-		move_down(speed)
+		inputMoveVector.y -= 1
 	else:
-		decelerate_z()
+		pass
+	
+	# LEFT AND RIGHT MOVEMENT
+	if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_right"):
+		pass
+	elif Input.is_action_pressed("move_left"):
+		inputMoveVector.x -= 1
+	elif Input.is_action_pressed("move_right"):
+		inputMoveVector.x += 1
+	else:
+		pass
 		
-	# DIAGONAL MOVEMENT
-	if Input.is_action_pressed("move_up") and Input.is_action_pressed("move_right"):
-		move_up_and_right(speed)
+	inputMoveVector = inputMoveVector.normalized()
+	dir += -camera_dir.z * inputMoveVector.y
+	dir += camera_dir.x * inputMoveVector.x
+	dir.y = 0
+	dir = dir.normalized()
 	
-	if Input.is_action_pressed("move_right") and Input.is_action_pressed("move_down"):
-		move_right_and_down(speed)
-	
-	if Input.is_action_pressed("move_down") and Input.is_action_pressed("move_left"):
-		move_down_and_left(speed)
-		
-	if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_up"):
-		move_left_and_up(speed)
-	
-	
-	
+	velocity.y = 0
+	var moveDir = dir * speed
+	var up = Vector3(0, 1, 0)
+	velocity = velocity.linear_interpolate(moveDir, ACCELERATION * delta)
+	velocity = move_and_slide(velocity, up, 0.05, 4, 45)
 	
 	# BALL MESH ROTATION ACCORDING TO SPEED
 	$MeshInstance.rotate_z(deg2rad(-velocity.x))
 	$MeshInstance.rotate_x(deg2rad(velocity.z))
 	# ACTUAL MOVEMENT
-	move_and_slide(velocity)
+	#move_and_slide(velocity)
 
 
 
 
 
 
-
-
-func decelerate_z():
-	velocity.z = lerp(velocity.z, 0, ACCELERATION)
 	
+
 func decelerate_x():
 	velocity.x = lerp(velocity.x, 0, ACCELERATION)
-
-func move_up(speed, is_original_event = true):
-	if (cameraTargetAngle == 315):
-		move_up_and_right(speed)
-	elif (cameraTargetAngle == 270):
-		move_right(speed)
-	elif(cameraTargetAngle == 225):
-		move_right_and_down(speed)
-	elif(cameraTargetAngle == 180):
-		if (is_original_event):
-			move_down(speed, false)
-		else:
-			velocity.z = lerp(velocity.z, -speed, ACCELERATION)
-	elif(cameraTargetAngle == 135):
-		move_down_and_left(speed)
-	elif(cameraTargetAngle == 90):
-		move_left(speed)
-	elif(cameraTargetAngle == 45):
-		move_left_and_up(speed)
-	else:
-		velocity.z = lerp(velocity.z, -speed, ACCELERATION)
-	
-func move_down(speed, is_original_event:bool = true):
-	if (cameraTargetAngle == 315):
-		move_down_and_left(speed)
-	elif (cameraTargetAngle == 270):
-		move_left(speed)
-	elif(cameraTargetAngle == 225):
-		move_left_and_up(speed)
-	elif(cameraTargetAngle == 180):
-		if(is_original_event):
-			move_up(speed, false)
-		else:
-			velocity.z = lerp(velocity.z, speed, ACCELERATION)
-	elif(cameraTargetAngle == 135):
-		move_up_and_right(speed)
-	elif(cameraTargetAngle == 90):
-		move_right(speed)
-	elif(cameraTargetAngle == 45):
-		move_right_and_down(speed)
-	else:
-		velocity.z = lerp(velocity.z, speed, ACCELERATION)
-	
-func move_left(speed, is_original_event = true):
-	velocity.x = lerp(velocity.x, -speed, ACCELERATION)
-	
-	
-func move_right(speed, is_original_event = true):
-	velocity.x = lerp(velocity.x, speed, ACCELERATION)
-	
-func move_up_and_right(speed, is_original_event = true):
-	var diagonalSpeed = speed / DIAGONAL_SCALE
-	velocity.z = lerp(velocity.z, -diagonalSpeed, ACCELERATION)
-	velocity.x = lerp(velocity.x, diagonalSpeed, ACCELERATION)
-	
-func move_right_and_down(speed, is_original_event = true):
-	var diagonalSpeed = speed / DIAGONAL_SCALE
-	velocity.x = lerp(velocity.x, diagonalSpeed, ACCELERATION)
-	velocity.z = lerp(velocity.z, diagonalSpeed, ACCELERATION)
-	
-func move_down_and_left(speed, is_original_event = true):
-	var diagonalSpeed = speed / DIAGONAL_SCALE
-	velocity.z = lerp(velocity.z, diagonalSpeed, ACCELERATION)
-	velocity.x = lerp(velocity.x, -diagonalSpeed, ACCELERATION)
-	
-func move_left_and_up(speed, is_original_event = true):
-	var diagonalSpeed = speed / DIAGONAL_SCALE
-	velocity.x = lerp(velocity.x, -diagonalSpeed, ACCELERATION)
-	velocity.z = lerp(velocity.z, -diagonalSpeed, ACCELERATION)
+#
+#func move_up(speed, is_original_event = true):
+#	velocity.z = lerp(velocity.z, -speed, ACCELERATION)
+#
+#func move_down(speed, is_original_event:bool = true):
+#	velocity.z = lerp(velocity.z, speed, ACCELERATION)
+#
+#func move_left(speed, is_original_event = true):
+#	velocity.x = lerp(velocity.x, -speed, ACCELERATION)
+#
+#func move_right(speed, is_original_event = true):
+#	velocity.x = lerp(velocity.x, speed, ACCELERATION)
+#
+#func move_up_and_right(speed, is_original_event = true):
+#	var diagonalSpeed = speed / DIAGONAL_SCALE
+#	velocity.z = lerp(velocity.z, -diagonalSpeed, ACCELERATION)
+#	velocity.x = lerp(velocity.x, diagonalSpeed, ACCELERATION)
+#
+#func move_right_and_down(speed, is_original_event = true):
+#	var diagonalSpeed = speed / DIAGONAL_SCALE
+#	velocity.x = lerp(velocity.x, diagonalSpeed, ACCELERATION)
+#	velocity.z = lerp(velocity.z, diagonalSpeed, ACCELERATION)
+#
+#func move_down_and_left(speed, is_original_event = true):
+#	var diagonalSpeed = speed / DIAGONAL_SCALE
+#	velocity.z = lerp(velocity.z, diagonalSpeed, ACCELERATION)
+#	velocity.x = lerp(velocity.x, -diagonalSpeed, ACCELERATION)
+#
+#func move_left_and_up(speed, is_original_event = true):
+#	var diagonalSpeed = speed / DIAGONAL_SCALE
+#	velocity.x = lerp(velocity.x, -diagonalSpeed, ACCELERATION)
+#	velocity.z = lerp(velocity.z, -diagonalSpeed, ACCELERATION)
